c9ab36c555af799c99646201bb192e81e46de070
Vural Kayra Cetintas <73638860+vuralkayracetintas@users.noreply.github.com>
2023-08-09 07:23:54 +0300

    generate model

--- lib/product/model/reddit_model.dart
+++ lib/product/model/reddit_model.dart
@@ -1,1 +1,79 @@
-class RedditModel {}
+class RedditModel {
+  RedditModel({
+    required this.kind,
+    required this.data,
+  });
+  late final String kind;
+  late final Data data;
+
+  RedditModel.fromJson(Map<String, dynamic> json) {
+    kind = json['kind'];
+    data = Data.fromJson(json['data']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['kind'] = kind;
+    _data['data'] = data.toJson();
+    return _data;
+  }
+}
+
+class Data {
+  Data({
+    this.after,
+    required this.dist,
+    required this.modhash,
+    required this.geoFilter,
+    required this.children,
+    required this.before,
+  });
+  late final Null after;
+  late final int dist;
+  late final String modhash;
+  late final String geoFilter;
+  late final List<Children> children;
+  late final String before;
+
+  Data.fromJson(Map<String, dynamic> json) {
+    after = null;
+    dist = json['dist'];
+    modhash = json['modhash'];
+    geoFilter = json['geo_filter'];
+    children =
+        List.from(json['children']).map((e) => Children.fromJson(e)).toList();
+    before = json['before'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['after'] = after;
+    _data['dist'] = dist;
+    _data['modhash'] = modhash;
+    _data['geo_filter'] = geoFilter;
+    _data['children'] = children.map((e) => e.toJson()).toList();
+    _data['before'] = before;
+    return _data;
+  }
+}
+
+class Children {
+  Children({
+    required this.kind,
+    required this.data,
+  });
+  late final String kind;
+  late final Data data;
+
+  Children.fromJson(Map<String, dynamic> json) {
+    kind = json['kind'];
+    data = Data.fromJson(json['data']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['kind'] = kind;
+    _data['data'] = data.toJson();
+    return _data;
+  }
+}
